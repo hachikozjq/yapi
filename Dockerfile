dc4132500811625ffe88ea5a0d09289dd0929db7
zhoujunquan <343122875@qq.com>
2023-07-19 18:08:25 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:12-alpine as builder
+WORKDIR /yapi
+ENV VERSION=1.12.0
+RUN wget https://github.com/YMFE/yapi/archive/v${VERSION}.zip
+RUN unzip v${VERSION}.zip && mv yapi-${VERSION} vendors
+WORKDIR /yapi/vendors
+RUN apk add python2 make
+RUN npm install --production --registry https://registry.npm.taobao.org
+RUN wget https://raw.githubusercontent.com/hachikozjq/yapi/main/entrypoint.sh
+RUN chmod a+x entrypoint.sh
+
+FROM node:12-alpine
+LABEL maintainer="343122875@qq.com"
+ENV TZ="Asia/Shanghai"
+WORKDIR /yapi/vendors
+COPY --from=builder /yapi/vendors /yapi/vendors
+RUN cp config_example.json /yapi/config.json
+EXPOSE 3000
+ENTRYPOINT ["sh", "entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,47 @@
-test
+#!/bin/sh
+#update config file with env var
+conf_file=/config.json
+if [ $YAPI_SERVER_PORT ]; then
+    sed -i 2c\"port\":\"$YAPI_SERVER_PORT\", $conf_file
+fi
+if [ $YAPI_ADMINACCOUNT ]; then
+    sed -i 4c\"adminAccount\":\"$YAPI_ADMINACCOUNT\", $conf_file
+fi
+if [ $YAPI_DB_SERVERNAME ]; then
+    sed -i 6c\"servername\":\"$YAPI_DB_SERVERNAME\", $conf_file
+fi
+if [ $YAPI_DB_DATABASE ]; then
+    sed -i 7c\"DATABASE\":\"$YAPI_DB_DATABASE\", $conf_file
+fi
+if [ $YAPI_DB_PORT ]; then
+    sed -i 8c\"port\":\"$YAPI_DB_PORT\", $conf_file
+fi
+#if [ $YAPI_DB_USER ]; then
+#    sed -i 9c\"user\":\"$YAPI_DB_USER\", $conf_file
+#fi
+#if [ $YAPI_DB_PASS ]; then
+#    sed -i 10c\"pass\":\"$YAPI_DB_PASS\", $conf_file
+#fi
+#if [ $YAPI_DB_AUTHSOURCE ]; then
+#    sed -i 11c\"authSource\":\"$YAPI_DB_AUTHSOURCE\" $conf_file
+#fi
+if [ $YAPI_MAIL_ENABLE ]; then
+    sed -i 11c\"enable\":$YAPI_MAIL_ENABLE, $conf_file
+fi
+if [ $YAPI_MAIL_HOST ]; then
+    sed -i 12c\"host\":\"$YAPI_MAIL_HOST\", $conf_file
+fi
+if [ $YAPI_MAIL_PORT ]; then
+    sed -i 13c\"port\":$YAPI_MAIL_PORT, $conf_file
+fi
+if [ $YAPI_MAIL_FROM ]; then
+    sed -i 14c\"from\":\"$YAPI_MAIL_FROM\", $conf_file
+fi
+if [ $YAPI_MAIL_USER ]; then
+    sed -i 16c\"user\":\"$YAPI_MAIL_USER\", $conf_file
+fi
+if [ $YAPI_MAIL_PASS ]; then
+    sed -i 17c\"pass\":\"$YAPI_MAIL_PASS\" $conf_file
+fi
+#start yapi
+node server/app.js
